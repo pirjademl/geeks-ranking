6809e7cf8e4e787eb725b9c25d5a4e5856bcf364
magdump <pirjademl7@gmail.com>
2024-10-19 17:23:23 +0530

    chore : add input component

--- components/input.tsx
+++ components/input.tsx
@@ -1,16 +1,29 @@
 import { FC } from "react";
 import { TextInput } from "react-native";
 interface InputProps {
-  placeholderText: string;
+    placeholderText: string;
+    isPassword?: boolean;
+    value: string;
+    onChangeHandler: (arg0: string) => void;
 }
 
-const Input: FC<InputProps> = ({ placeholderText }) => {
-  return (
-    <TextInput
-      className="bg-primary border border-border px-3 py-1 rounded-sm mt-2 text-secondary"
-      placeholderTextColor="#444"
-      placeholder={placeholderText}
-    />
-  );
+const Input: FC<InputProps> = ({
+    placeholderText,
+    isPassword = false,
+    onChangeHandler,
+    value,
+}) => {
+    return (
+        <TextInput
+            className="bg-primary border border-border px-3 py-1 rounded-sm mt-2 text-secondary font-bold text-sm"
+            placeholderTextColor="#444"
+            placeholder={placeholderText}
+            secureTextEntry={isPassword}
+            value={value}
+            onChangeText={onChangeHandler}
+        />
+    );
 };
+Input.displayName = "InputField";
+
 export { Input };
